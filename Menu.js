b21629541f42ac7e506ddbabec0a2340bcf54a44
Kacper2987 <150734327+Kacper2987@users.noreply.github.com>
2026-03-11 21:56:51 +0100

    Add files via upload

--- Menu.js
+++ Menu.js
@@ -8,3 +8,27 @@ nav.classList.toggle("active");
 });
 
 });
+
+
+
+let lastScroll = 0;
+const header = document.querySelector(".site-header");
+
+window.addEventListener("scroll", () => {
+
+if(window.innerWidth <= 768){
+
+let currentScroll = window.pageYOffset;
+
+if(currentScroll > lastScroll){
+header.style.transform = "translateX(-100%)";
+}
+else{
+header.style.transform = "translateX(0)";
+}
+
+lastScroll = currentScroll;
+
+}
+
+});
